e593b284983af755a45b8efdf2fc7081680d35de
129juandiego <75152176+129juandiego@users.noreply.github.com>
2020-12-02 13:43:03 -0600

    Update ic_0301_04_10_2020_Juan_diego_mateos_alvarado_aap.c

--- M simplex/ic_0301_04_10_2020_Juan_diego_mateos_alvarado_aap.c
+++ M simplex/ic_0301_04_10_2020_Juan_diego_mateos_alvarado_aap.c
@@ -1,5 +1,10 @@
 #include "ic_0301_04_10_2020_Juan_diego_mateos_alvarado_simplex.h"
-
+/**
+@brief declaramos un valor de tipo entero llamado leer datos 
+@param esto nos dara un valor entero llamado var 
+@param declaramos los  valores de tipo entero rest, c, i que seran donde guardaremos los datos que pida el programa
+@param mandamos a llamar la funcion que se optimizara esta la ingresara el usuario
+*/
 int leeDatos(int *var){
 int rest,c,i;
 
@@ -9,7 +14,15 @@ int rest,c,i;
 	scanf("%d",var);
 	
 	printf("\ntoma en cuenta que: c1.X1+c2.X2+...cn.Xn = b \n\n");
-	
+	/**
+	@param tomamos en cuenta la formula del metodo simplex 
+        @param declaramos un for para que realize la  operacion
+	@param c es la funcion 
+	@param rest el numero de restricciones 
+	@param i es el termino independiente 
+	@paramestos datos se le piden a el usuario y se guardan en rest,c,i y var que serviran para sacar las matrizes
+	@return esto nos retornara un rest
+	*/
 	for(c=0;c<*var;c++)
 	{
 		
